a9fd1940dd770a8b1bf64ccf796557ba555e3bbc
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-03 17:46:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,10 +67,10 @@ class Student
   end
 
    
-  # def update
-  #   sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
-  #   DB[:conn].execute(sql, self.name, self.grade, self.name)
-  # end
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.name)
+  end
 
 
 end
